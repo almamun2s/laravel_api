8704f5cc9979eab2af7bd9d4efd8bab0c67bb2ca
Abdullah Almamun <mamun2social@gmail.com>
2025-03-11 14:38:05 +0600

    feat: customer resource created

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->namespace('App\Http\Controllers\Api\V1')->group(function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+});
